9fe47852dc3313eebad8a475862d24cff31eb9cb
Michael Mogessie <michaelmogessie@gmail.com>
2021-12-14 18:50:31 -0500

    TRAINSPOTTING-010

--- discoveryservice/deploy.sh
+++
@@ -1,5 +0,0 @@
-mvn clean install -DskipTests
-docker build -t michaelmogessie/discovery-service-docker .
-docker push michaelmogessie/discovery-service-docker:latest
-kubectl delete po discovery-service-deployment || true
-kubectl create -f discovery_service_deployment_def.yml
