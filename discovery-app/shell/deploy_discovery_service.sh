85b403363d399500ce78fb43da2f7afd5d59ff66
Michael Mogessie <michaelmogessie@gmail.com>
2021-12-17 17:57:12 -0500

    TRAINSPOTTING-010

--- discovery-app/shell/deploy_discovery_service.sh
+++ discovery-app/shell/deploy_discovery_service.sh
@@ -1,2 +1,2 @@
-kubectl delete svc discovery-service || true
+kubectl delete svc discovery-service -n michaelmogessie || true
 kubectl create -f /home/michael/git/trainspotting/discovery-app/k8s/discovery_service_def.yml
--- train-scheduler-app/src/main/java/info/michaelmogessie/scheduler/controllers/ScheduleController.java
+++ train-scheduler-app/src/main/java/info/michaelmogessie/scheduler/controllers/ScheduleController.java
@@ -14,34 +14,54 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import info.michaelmogessie.scheduler.pojos.Schedule;
+import info.michaelmogessie.scheduler.pojos.Train;
 import info.michaelmogessie.scheduler.repositories.ScheduleRepository;
 
 @RestController
 public class ScheduleController {
-    @Autowired
-    private ScheduleRepository scheduleRepository;
+        @Autowired
+        private ScheduleRepository scheduleRepository;
+        @Autowired
+        private WebClient.Builder webClientBuilder;
 
-    @GetMapping(value = "/schedules")
-    public ResponseEntity<CollectionModel<EntityModel<Schedule>>> getSchedules() {
-        List<EntityModel<Schedule>> schedules = StreamSupport.stream(scheduleRepository.findAll().spliterator(), false)
-                .map(schedule -> EntityModel.of(schedule,
-                        linkTo(methodOn(ScheduleController.class).getSchedule(schedule.getScheduleId())).withSelfRel(),
-                        linkTo(methodOn(ScheduleController.class).getSchedules()).withRel("schedules")))
-                .collect(Collectors.toList());
+        @GetMapping(value = "/schedules")
+        public ResponseEntity<CollectionModel<EntityModel<Schedule>>> getSchedules() {
+                List<EntityModel<Schedule>> schedules = StreamSupport
+                                .stream(scheduleRepository.findAll().spliterator(), false)
+                                .map(schedule -> EntityModel.of(schedule,
+                                                linkTo(methodOn(ScheduleController.class)
+                                                                .getSchedule(schedule.getScheduleId())).withSelfRel(),
+                                                linkTo(methodOn(ScheduleController.class).getSchedules())
+                                                                .withRel("schedules")))
+                                .collect(Collectors.toList());
 
-        return ResponseEntity.ok(
-                CollectionModel.of(schedules, linkTo(methodOn(ScheduleController.class).getSchedules()).withSelfRel()));
-    }
+                return ResponseEntity.ok(
+                                CollectionModel.of(schedules, linkTo(methodOn(ScheduleController.class).getSchedules())
+                                                .withSelfRel()));
+        }
 
-    @GetMapping("/schedules/{scheduleId}")
-    public ResponseEntity<EntityModel<Schedule>> getSchedule(@PathVariable int scheduleId) {
-        return scheduleRepository.findById(scheduleId)
-                .map(schedule -> EntityModel.of(schedule,
-                        linkTo(methodOn(ScheduleController.class).getSchedule(schedule.getScheduleId())).withSelfRel(),
-                        linkTo(methodOn(ScheduleController.class).getSchedules()).withRel("schedules")))
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
-    }
+        @GetMapping("/schedules/{scheduleId}")
+        public ResponseEntity<EntityModel<Schedule>> getSchedule(@PathVariable int scheduleId) {
+                return scheduleRepository.findById(scheduleId)
+                                .map(schedule -> EntityModel.of(schedule,
+                                                linkTo(methodOn(ScheduleController.class)
+                                                                .getSchedule(schedule.getScheduleId())).withSelfRel(),
+                                                linkTo(methodOn(ScheduleController.class).getSchedules())
+                                                                .withRel("schedules")))
+                                .map(ResponseEntity::ok)
+                                .orElse(ResponseEntity.notFound().build());
+        }
+
+        @GetMapping(value = "/trains")
+        public ResponseEntity<CollectionModel<EntityModel<Train>>> getTrains() {
+                return ResponseEntity.ok(CollectionModel.of(webClientBuilder.build().get()
+                                .uri("http://train-management-service/trains")
+                                .retrieve().bodyToFlux(Train.class)
+                                .collectList().share().block().stream()
+                                .map(train -> EntityModel.of(train))
+                                .collect(Collectors.toList())));
+        }
 }
