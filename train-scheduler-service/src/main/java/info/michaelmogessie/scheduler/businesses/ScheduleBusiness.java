028102d03c61ce79910d122e64675670de119a9a
Michael Mogessie <michaelmogessie@gmail.com>
2021-12-29 17:11:34 -0500

    TRAINSPOTTING-010

--- train-scheduler-service/src/main/java/info/michaelmogessie/scheduler/businesses/ScheduleBusiness.java
+++ train-scheduler-service/src/main/java/info/michaelmogessie/scheduler/businesses/ScheduleBusiness.java
@@ -26,29 +26,6 @@ public class ScheduleBusiness {
         schedule.getDepartures().forEach(checkPoint -> checkPoint
                 .setStation(stationRepository.findById(checkPoint.getStation().getStationId()).get()));
         schedule.setTrain(trainRepository.findById(schedule.getTrain().getTrainId()).get());
-        // Map<Integer, Station> stationMap = schedule.getStations().stream()
-        // .collect(Collectors.toMap(Station::getStationId, Function.identity()));
-        // List<Station> stations = new ArrayList<>();
-        // stationMap.entrySet().stream()
-        // .map(station -> Map.of(stationRepository.findById(station.getKey()),
-        // station.getValue()))
-        // .forEach(station -> {
-        // for (Map.Entry<Optional<Station>, Station> s : station.entrySet()) {
-        // if (s.getKey().isPresent()) {
-        // stations.add((Station) s.getKey().get());
-        // } else {
-        // stations.add((Station) s.getValue());
-        // }
-        // break;
-        // }
-        // });
-        // schedule.setStations(stations);
-
-        // Optional<Train> train =
-        // trainRepository.findById(schedule.getTrain().getTrainId());
-        // if (train.isPresent()) {
-        // schedule.setTrain(train.get());
-        // }
         return scheduleRepository.save(schedule);
     }
 }
