cc7196fe72ab6b77f7a75d6370a56178d779f133
Michael Mogessie <michaelmogessie@gmail.com>
2021-12-29 22:40:25 -0500

    TRAINSPOTTING-010

---
+++ train-scheduler-app/src/app/materials/materials.module.ts
@@ -0,0 +1,60 @@
+import { NgModule } from '@angular/core';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatChipsModule } from '@angular/material/chips';
+
+
+
+const modules: any[] = [
+  LayoutModule,
+  MatAutocompleteModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatToolbarModule,
+  MatExpansionModule,
+  MatIconModule,
+  MatButtonModule,
+  MatDividerModule,
+  MatGridListModule,
+  MatDialogModule,
+  MatTabsModule,
+  MatSnackBarModule,
+  MatChipsModule,
+  FormsModule,
+  ReactiveFormsModule,
+  MatNativeDateModule,
+  MatProgressSpinnerModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [...modules],
+  exports: [...modules]
+})
+export class MaterialsModule { }
